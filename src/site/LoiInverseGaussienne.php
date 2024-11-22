15e6eb6ad489e6c43eeaa43b0b67f615ad3da4ed
Abdelaziz Chaaraoui <22403728@me.uvsq.fr>
2024-11-22 10:09:44 +0100

    commencement module de proba

--- src/site/LoiInverseGaussienne.php
+++ src/site/LoiInverseGaussienne.php
@@ -2,3 +2,16 @@
 session_start();
 require("header_module.php");
 header_page("Loi inverse Gaussienne");
+echo "
+<form method='POST' class='boutons_tableau_de_bord'>
+<input type='number' class='bouton' name='x' placeholder='x'>
+<input type='number' class='bouton' name='x' placeholder='λ'>
+<input type='number' class='bouton' name='x' placeholder='μ'>
+<input type='submit' class='bouton' name='methode1' value='Méthode des rectangles gauches'>
+<input type='submit' class='bouton' name='methode2' value='Méthode des rectangles droits'>
+<input type='submit' class='bouton' name='methode3' value='Méthode des rectangles médians'>
+<input type='submit' class='bouton' name='methode4' value='Méthode des rectangles trapèzes'>
+<input type='submit' class='bouton' name='methode5' value='Méthode de Simpson'>
+</form>
+</body>";
+
