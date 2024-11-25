195efb9c2e3ee551bd4ae143dec128a2f76ba763
Abdelaziz Chaaraoui <22403728@me.uvsq.fr>
2024-11-25 14:43:01 +0100

    courbe loi inverse gaussienne faite

--- src/site/fonctionsLIG.php
+++ src/site/fonctionsLIG.php
@@ -2,7 +2,7 @@
 function loi_inverse_gaussienne($x_values, $esperance, $forme) {
     $densities = [];
     foreach ($x_values as $x) {
-        if ($x < 0) {
+        if ($x <= 0) {
             // Avoid division by zero by setting density to 0 for x <= 0
             $density = 0;
         } else {
@@ -14,14 +14,4 @@ function loi_inverse_gaussienne($x_values, $esperance, $forme) {
 }
 
 
-function methode_Des_Rectangles_Median($x_values, $esperance, $forme, $a, $b, $n) {
-    $resultat = 0;
-    for($i = 0; $i < $n-1; $i++){
-        if($x_values[$i+1]){
-            $resultat += loi_inverse_gaussienne(($x_values[$i] + $x_values[$i+1])/2, $esperance, $forme);
 
-        }
-    }
-
-    return (($b-$a)/$n)*$resultat;
-}
