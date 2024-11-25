ec71156eb42d752c9a2a106eb37ad5a42c04a7ee
Abdelaziz Chaaraoui <22403728@me.uvsq.fr>
2024-11-25 14:41:16 +0100

    courbe loi inverse gaussienne faite

--- src/site/LoiInverseGaussienne.php
+++ src/site/LoiInverseGaussienne.php
@@ -1,15 +1,88 @@
 <?php
 session_start();
 require("header_module.php");
+require("fonctionsLIG.php");
 header_page("Loi inverse Gaussienne");
+
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 echo "
 <form method='POST' class='boutons_tableau_de_bord'>
 <input type='number' class='bouton' name='x' placeholder='x'>
-<input type='number' class='bouton' name='x' placeholder='λ'>
-<input type='number' class='bouton' name='x' placeholder='μ'>
+<input type='number' class='bouton' name='forme' placeholder='λ'>
+<input type='number' class='bouton' name='esperance' placeholder='μ'>
 <input type='submit' class='bouton' name='methode1' value='Méthode des rectangles médians'>
 <input type='submit' class='bouton' name='methode2' value='Méthode des rectangles trapèzes'>
 <input type='submit' class='bouton' name='methode3' value='Méthode de Simpson'>
 </form>
-</body>";
+";
 
+if (isset($_POST['methode1'], $_POST['x'], $_POST['forme'], $_POST['esperance'])) {
+    $x = $_POST['x'];
+    $forme = $_POST['forme'];
+    $esperance = $_POST['esperance'];
+
+    // Generate a range of x values for the plot
+    $x_values = range(0, $x, 0.1);
+    $densities = loi_inverse_gaussienne($x_values, $forme, $esperance);
+    // Debugging: Print the values to check if they are correct
+    echo "<pre>";
+    print_r($x_values);
+    print_r($densities);
+    echo "</pre>";
+
+    // Encode the data as JSON
+    $x_values_json = json_encode($x_values);
+    $densities_json = json_encode($densities);
+
+    echo "
+    <canvas id='myChart' width='400' height='200'></canvas>
+    <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+
+            // Debugging: Print the values to check if they are correct
+            console.log($x_values_json);
+            console.log($densities_json);
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: $x_values_json,
+                    datasets: [{
+                        label: 'Loi inverse Gaussienne',
+                        data: $densities_json,
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1,
+                        fill: false
+                    },
+                  
+                    ]
+                },
+                options: {
+                    scales: {
+                        x: {
+                            title: {
+                                display: true,
+                                text: 'x'
+                            }
+                        },
+                        y: {
+                            title: {
+                                display: true,
+                                text: 'Densité'
+                            }
+                        }
+                    }
+                }
+            });
+        });
+    </script>
+    ";
+}
+
+?>
--- src/site/fonctionsLIG.php
+++ src/site/fonctionsLIG.php
@@ -1,21 +1,24 @@
 <?php
-function loi_inverse_gaussienne($x, $esperance, $forme) {
-    $densite = array();
-    foreach ($x as $value) {
-        if ($value < 0) {
-            $densite[] = 0;
+function loi_inverse_gaussienne($x_values, $esperance, $forme) {
+    $densities = [];
+    foreach ($x_values as $x) {
+        if ($x < 0) {
+            // Avoid division by zero by setting density to 0 for x <= 0
+            $density = 0;
         } else {
-            $densite[] = sqrt($forme / (2 * M_PI * pow($value, 3))) * exp(-$forme * pow(($value - $esperance), 2) / (2 * pow($esperance, 2) * $value));
+            $density = sqrt($forme / (2 * pi() * pow($x, 3))) * exp(-($forme * pow($x - $esperance, 2)) / (2 * pow($esperance, 2) * $x));
         }
+        $densities[] = $density;
     }
-    return $densite;
+    return $densities;
 }
 
-function methode_Des_Rectangles_Median($points, $esperance, $forme, $a, $b, $n) {
+
+function methode_Des_Rectangles_Median($x_values, $esperance, $forme, $a, $b, $n) {
     $resultat = 0;
     for($i = 0; $i < $n-1; $i++){
-        if($points[$i+1]){
-            $resultat += loi_inverse_gaussienne(($points[$i] + $points[$i+1])/2, $esperance, $forme);
+        if($x_values[$i+1]){
+            $resultat += loi_inverse_gaussienne(($x_values[$i] + $x_values[$i+1])/2, $esperance, $forme);
 
         }
     }
