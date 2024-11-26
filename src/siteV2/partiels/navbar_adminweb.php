2d73836c5f31ab636b2c3c3b6fe5ae73cea995c2
younes <younesazirgui@gmail.com>
2024-11-26 23:15:21 +0100

    creation d'un navbar pour l'admin systeme

---
+++ src/siteV2/partiels/navbar_adminweb.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap">
+
+</head>
+<body>
+
+
+<!-- navbar section starts here -->
+ <section class="navbar text-center">
+    <div class="container">
+            <img src="images/sigmaXlogo.png" alt="logo" class="logo img-responsive">
+        <ul>
+            <li><a href="adminweb.php">Accueil adminweb</a></li>
+            <li><a href="inscritcsv2.php">Utilisateurs inscrits</a></li>
+            <li><a href="gestion_compte.php">Gestion de comptes</a></li>
+            <li><a href="accueil_non_inscrit.php">Se déconnecter</a></li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+ </section>
+<!-- navbar section ends here -->
