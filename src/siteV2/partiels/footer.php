4e1740a1815efbbd3bdc78bc5da46bee1a0daa94
younes <younesazirgui@gmail.com>
2024-11-26 23:19:00 +0100

    modification du footer

--- src/siteV2/partiels/footer.php
+++ src/siteV2/partiels/footer.php
@@ -1,6 +1,7 @@
  <!-- social Section Starts Here -->
  <section class="social">
         <div class="container text-center">
+        <!--   
             <ul>
                 <li>
                     <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
@@ -12,6 +13,7 @@
                     <a href="https://x.com/younnss95"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
                 </li>
             </ul>
+            -->
             <a href="accueil.php"><img src="images/sigmaXlogo.png" alt="SigmaBot" class="SigmaBot"></a>
             <p>All rights reserved. Created By Abdelaziz, Kevin, Pierre-Alix, Rayan and Younes</p>
         </div>
