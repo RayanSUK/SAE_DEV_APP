181ab27dc0ce56004d9d6839b04d39535bf925ca
Pierre-Alix <126257778+Pierre-Alix@users.noreply.github.com>
2024-11-29 10:17:39 +0100

    modif. apportée - fonctionne maintenant avec la BDD
    -Enregistrement des infos du formulaire dans la base de données
    -Affichage de messages d'erreurs si :
          - L'utilisateur rate le captcha
          - Le login du nouvel inscrit existe déjà

--- src/siteV2/inscription.php
+++ src/siteV2/inscription.php
@@ -7,39 +7,72 @@ if (!isset($_SESSION['captcha'])) {
     shuffle($tab);
     $_SESSION['captcha'] = $tab[0];
 }
+
 $nb = $_SESSION['captcha'];
 
-// Créez le fichier CSV si nécessaire
-if (!file_exists('utilisateurs.csv')) {
-    $fp = fopen('utilisateurs.csv', 'w');
-    fputcsv($fp, ['ID', 'PASSWORD', 'CAPTCHA', 'IP', 'DATE']); // En-tête
-    fclose($fp);
-}
 
 // Traitement du formulaire d'inscription
 if (isset($_POST['nom'], $_POST['mdp'], $_POST['reponse'])) {
     $nom = $_POST['nom'];
-    $mdp = $_POST['mdp'];
+    $mdp = md5($_POST['mdp']);
     $reponse = $_POST['reponse'];
     $ip = $_SERVER['REMOTE_ADDR'];
     $date = date("Y-m-d H:i:s");
 
-    if ($reponse == $_SESSION['captcha']) {
-        // Enregistrer dans le fichier CSV sans hachage
-        $fp = fopen('utilisateurs.csv', 'a');
-        fputcsv($fp, [$nom, $mdp, $ip, $date]);
-        fclose($fp);
 
-        $_SESSION['nom'] = $nom;
-        $_SESSION['etat'] = 'inscription';
+
+    if ($reponse == $_SESSION['captcha']) {
+        // Connexion à la base de données
+        $cnx = mysqli_connect('localhost', 'root', '', 'sigmax');
+        if (!$cnx) {
+            die("Échec de la connexion à la base de données: " . mysqli_connect_error());
+        }
+
+
+        // Vérifie si le login est déjà utilisé
+        $check_sql = "SELECT * FROM USERS WHERE login = ?";
+        $check_stmt = mysqli_prepare($cnx, $check_sql);
+        mysqli_stmt_bind_param($check_stmt, "s", $nom);
+        mysqli_stmt_execute($check_stmt);
+        $check_result = mysqli_stmt_get_result($check_stmt);
+
+
+        if (mysqli_num_rows($check_result) > 0) {
+            // Le login est déjà utilisé : renvoie un message d'erreur et l'utilisateur doit recommencer
+            header("Location:inscription.php");
+            $_SESSION['error'] = "Login déjà utilisé.";
+
+        } else {
+            // Insére dans la base de données
+            $sql = "INSERT INTO USERS (login, password) VALUES (?, ?)";
+            $stmt = mysqli_prepare($cnx, $sql);
+            mysqli_stmt_bind_param($stmt, "ss", $nom, $mdp);
+
+            if (mysqli_stmt_execute($stmt)) {
+                // Message affiché si l'inscription est réussie
+                echo "<p style='color:green;'>Inscription validée !</p>";
+            } else {
+                // Message affiché en cas d'erreurs
+                $_SESSION['error'] = "Erreur lors de l'inscription.";
+                header("Location:inscription.php");
+            }
+        }
+
+        // Fermeture des requêtes préparées
+        mysqli_stmt_close($check_stmt);
+        mysqli_stmt_close($stmt);
+        mysqli_close($cnx);
         unset($_SESSION['captcha']); // Supprimer le CAPTCHA après utilisation
         header("Location: accueil.php");
         exit;
     } else {
         echo "<p style='background-color: red; color: white;'>Captcha incorrect.</p>";
     }
+    session_unset();
 }
 ?>
+
+
 
 
 <!-- Formulaire d'inscription -->
@@ -66,6 +99,13 @@ if (isset($_POST['nom'], $_POST['mdp'], $_POST['reponse'])) {
                         <?php endforeach; ?>
                     </tr>
                 </table>
+                <br>
+                <?php
+                if (isset($_SESSION['error'])) {
+                    echo '<p style="color: red;">' . $_SESSION['error'] . '</p>';
+                    unset($_SESSION['error']);
+                }
+                ?>
             </form>
         </div>
     </div>
