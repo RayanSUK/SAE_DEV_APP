701f6bd9a96e4a82e75f0e2ee44427ccfa891c76
younes <younesazirgui@gmail.com>
2024-12-03 11:01:45 +0100

    ajout d'un template pour la future video

--- src/siteV2/accueil_non_inscrit.php
+++ src/siteV2/accueil_non_inscrit.php
@@ -5,23 +5,14 @@
 <!-- la section accueil commence ici-->
 <section class="text-center">
     <div class="titre text-center">
-        <h1>Video de présentation du site SigmaX</h1>
+    <h1>Pour pouvoir utiliser le site veuillez vous inscrire ou vous connecter</h1>
     </div>
 
     <div class="video">
-        <iframe width='1080' height='560' src='https://www.youtube.com/embed/G5RpJwCJDqc' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+    <img width="1080" height="560" src="images/Vidéo de présentation du site SigmaX.png" alt="Cliquez pour voir la vidéo">
     </div>
  </section>
 <!-- la section accueil se termine ici-->
-
-<!-- la section explication commence ici-->
-<section class="text-center">
-    <div class="explication">
-        <h1>Pour pouvoir utiliser le site veuillez vous inscrire ou vous connecter</h1>
-    </div>
-</section>
-
-<!-- la section explication se termine ici-->
 
 
 
