59ba320d0d2df68b432d5e0bb5e2efe51c38a019
RayanSUK <162138316+RayanSUK@users.noreply.github.com>
2024-12-06 15:21:01 +0100

    Update navbar_module1.php

--- src/siteV2/partiels/navbar_module1.php
+++ src/siteV2/partiels/navbar_module1.php
@@ -21,7 +21,7 @@
             <li><a href="module1.php">Module probabilité</a></li>
             <li><a href="module2.php">Module 2</a></li>
             <li><a href="module3.php">Module 3</a></li>
-            <li><a href="accueil_non_inscrit.php">Se déconnecter</a></li>
+            <li><a href="index.php">Se déconnecter</a></li>
         </ul>
         <div class="clearfix"></div>
     </div>
