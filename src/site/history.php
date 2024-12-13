3ae0b7c791875753f877eea6856323f038c0677f
abd <22403728@me.uvsq.fr>
2024-12-13 09:42:16 +0100

    correction suppression

--- src/site/history.php
+++ src/site/history.php
@@ -116,6 +116,8 @@ if(isset($_POST['supp']) && isset($_POST['id'])){
     mysqli_stmt_bind_param($stmt, "i", $id);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
 }
 
 if (isset($_POST['courbe']) && isset($_POST['id'])) {
@@ -125,7 +127,7 @@ if (isset($_POST['courbe']) && isset($_POST['id'])) {
     mysqli_stmt_bind_param($stmt, "i", $id);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    header("Location: " . $_SERVER['PHP_SELF']);
+}
 
     if ($row = mysqli_fetch_assoc($result)) {
         $points = array();
