1657bb6a978e0aed7744c77f59a18d156ac7270e
younss95 <163010503+younss95@users.noreply.github.com>
2025-01-09 14:46:59 +0100

    Update gestion_compte.php

--- src/site/gestion_compte.php
+++ src/site/gestion_compte.php
@@ -58,6 +58,7 @@ mysqli_close($cnx);
 <h1>Ajouter un nouvel utilisateur</h1>
 
 <!-- Formulaire pour ajouter un utilisateur -->
+<div class="text-center">
 <form action="gestion_compte.php" method="POST">
     <label for="login">Login:</label>
     <input type="text" id="login" name="login" required><br><br>
@@ -67,6 +68,7 @@ mysqli_close($cnx);
 
     <input type="submit" value="Ajouter utilisateur">
 </form>
+</div>
 
 <!-- Inclure le pied de page après le formulaire -->
 <?php include('partiels/footer.php'); ?>
