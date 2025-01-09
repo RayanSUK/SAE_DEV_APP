39699e0b4235500427dbfb27c425f233f225a725
younss95 <163010503+younss95@users.noreply.github.com>
2025-01-09 14:19:58 +0100

    ajout du text center au message d'erreur

--- src/site/connexion.php
+++ src/site/connexion.php
@@ -61,7 +61,7 @@ if (isset($_POST['nom'], $_POST['mdp'], $_POST['acces'])) {
             }
             exit;
         } else {
-            echo "<p style='color:red;'>Mot de passe incorrect.</p>";
+            echo "<p style='color:red;' class="text-center">Mot de passe incorrect.</p>";
         }
     } else {
         echo "<p style='color:red;'>Utilisateur non trouvé.</p>";
