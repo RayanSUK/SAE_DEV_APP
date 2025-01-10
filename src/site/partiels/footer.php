b53eb759ab4ea9f1ec51a1419ea0bd55eaeb85af
Pierre-Alix <126257778+Pierre-Alix@users.noreply.github.com>
2025-01-10 11:49:51 +0100

    Update footer.php

--- src/site/partiels/footer.php
+++ src/site/partiels/footer.php
@@ -14,7 +14,7 @@
                 </li>
             </ul>
             -->
-            <a href="accueil.php"><img src="images/sigmaXlogo.png" alt="SigmaBot" class="SigmaBot"></a>
+            <a><img src="images/sigmaXlogo.png" alt="SigmaBot" class="SigmaBot"></a>
             <p>All rights reserved. Created By Abdelaziz, Kevin, Pierre-Alix, Rayan and Younes</p>
         </div>
     </section>
