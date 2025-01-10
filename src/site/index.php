b8cd0380f89ceb8670dafcb28439eb20646b53cc
Pierre-Alix <126257778+Pierre-Alix@users.noreply.github.com>
2025-01-10 11:44:11 +0100

    Ajout de la vidéo de présentation sur la page d'accueil

--- src/site/index.php
+++ src/site/index.php
@@ -1,28 +1,24 @@
 <?php include('partiels/navbar_Inscription.php') ?>
 
-
-
 <!-- la section accueil commence ici-->
 <section class="text-center">
     <div class="titre text-center">
-    <h1>Pour pouvoir utiliser le site veuillez vous inscrire ou vous connecter</h1>
+        <h1>Pour pouvoir utiliser le site veuillez vous inscrire ou vous connecter</h1>
     </div>
+
+    <br><br><br>
 
     <div class="video">
-    <img width="1080" height="560" src="images/Vidéo de présentation du site SigmaX.png" alt="Cliquez pour voir la vidéo">
+        <iframe width="1000" height="500" src="https://www.youtube.com/embed/I5V0OazeDuU?si=miyL5ohuYoQ1xtjY" 
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+        </iframe>
     </div>
- </section>
+</section>
 <!-- la section accueil se termine ici-->
 
+<br><br><br>
 
+<?php include('partiels/footer.php') ?>
 
-
-
-
- <?php include('partiels/footer.php') ?>
-
-
-
-    
-</body>  
+</body>
 </html>
