74399f7bd819d243804b52b304230036977aa250
younes <younesazirgui@gmail.com>
2025-01-31 21:25:27 +0100

    ajout de main

--- src/site/index.php
+++ src/site/index.php
@@ -1,6 +1,7 @@
 <?php include('partiels/navbar_Inscription.php') ?>
 
 <!-- la section accueil commence ici-->
+<main role="main">
 <section class="text-center">
     <div class="titre text-center">
         <h1>Pour pouvoir utiliser le site veuillez vous inscrire ou vous connecter</h1>
@@ -9,11 +10,13 @@
     <br><br><br>
 
     <div class="video">
-        <iframe width="1000" height="500" src="https://www.youtube.com/embed/I5V0OazeDuU?si=miyL5ohuYoQ1xtjY" 
-                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+
+        <iframe width="1000" height="500" src="https://www.youtube.com/embed/I5V0OazeDuU?si=miyL5ohuYoQ1xtjY"
+                title="Video de presentation du site sigmax" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
         </iframe>
     </div>
 </section>
+</main>
 <!-- la section accueil se termine ici-->
 
 <br><br><br>
