21e820da256ce57f40af6832105d41fd8ff854b6
RayanSUK <162138316+RayanSUK@users.noreply.github.com>
2025-02-09 14:15:03 +0100

    modification de la redirection pour le module1

--- src/site/partiels/navbar.php
+++ src/site/partiels/navbar.php
@@ -17,7 +17,7 @@
             <img src="images/sigmaXlogo.png" alt="Le logo du site SigmaX" class="logo img-responsive">
         <ul>
             <li><a href="accueil.php">Accueil</a></li>
-            <li><a href="module1.php">Module probabilité</a></li>
+            <li><a href="LoiInverseGaussienne.php">Module probabilité</a></li>
             <li><a href="module2.php">Module 2</a></li>
             <li><a href="module3.php">Module 3</a></li>
             <li><a href="history.php">Historique des résultats</a></li>
