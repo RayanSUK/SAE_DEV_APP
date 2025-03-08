ff2e9590272e12b96a970860e978bb3b61a15475
Pierre-Alix <126257778+Pierre-Alix@users.noreply.github.com>
2025-03-08 18:43:58 +0100

    Ajout de la fonction RC4
    Permet le chiffrement et déchiffrement des mots de passes en utilisant RC4

--- src/site/fonctions.php
+++ src/site/fonctions.php
@@ -10,4 +10,28 @@ function Captcha() {
     // Retourner l'affichage de l'addition
     return [$num1, $num2];
 }
+
+function rc4($key, $data) {
+    $key = array_values(unpack('C*', $key));
+    $data = array_values(unpack('C*', $data));
+    $S = range(0, 255);
+    $j = 0;
+    
+    // Key Scheduling Algorithm (KSA)
+    for ($i = 0; $i < 256; $i++) {
+        $j = ($j + $S[$i] + $key[$i % count($key)]) % 256;
+        [$S[$i], $S[$j]] = [$S[$j], $S[$i]];
+    }
+    
+    // Pseudo-Random Generation Algorithm (PRGA)
+    $i = $j = 0;
+    $output = '';
+    foreach ($data as $byte) {
+        $i = ($i + 1) % 256;
+        $j = ($j + $S[$i]) % 256;
+        [$S[$i], $S[$j]] = [$S[$j], $S[$i]];
+        $output .= chr($byte ^ $S[($S[$i] + $S[$j]) % 256]);
+    }
+    return $output;
+}
 ?>
