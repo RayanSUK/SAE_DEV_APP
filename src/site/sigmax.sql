50050e0eb22895ac9027b0f17664cee049874d1a
Pierre-Alix <126257778+Pierre-Alix@users.noreply.github.com>
2025-03-21 06:16:52 +0100

    Nouvelles tables dans la base de données pour l'historique

--- src/site/sigmax.sql
+++ src/site/sigmax.sql
@@ -1,12 +1,9 @@
-CREATE DATABASE IF NOT EXISTS sigmax;
-USE sigmax;
-
 -- phpMyAdmin SQL Dump
 -- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : dim. 09 mars 2025 à 14:38
+-- Généré le : ven. 21 mars 2025 à 05:14
 -- Version du serveur : 5.7.24
 -- Version de PHP : 8.0.1
 
@@ -23,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `sigmax`
 --
+CREATE DATABASE IF NOT EXISTS `sigmax` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `sigmax`;
 
 -- --------------------------------------------------------
 
@@ -44,6 +43,21 @@ INSERT INTO `cle` (`cle_rc4`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `crypto`
+--
+
+CREATE TABLE `crypto` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `cle` varchar(255) NOT NULL,
+  `texte` varchar(255) NOT NULL,
+  `id_user` int(4) NOT NULL,
+  `date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `history`
 --
 
@@ -56,6 +70,28 @@ CREATE TABLE `history` (
   `esperance` double NOT NULL,
   `t` double NOT NULL,
   `resultat` double NOT NULL,
+  `id_user` int(4) NOT NULL,
+  `date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `polynomial`
+--
+
+CREATE TABLE `polynomial` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `a` int(255) NOT NULL,
+  `b` int(255) NOT NULL,
+  `c` int(4) NOT NULL,
+  `delta` int(4) NOT NULL,
+  `unique` varchar(255) NOT NULL,
+  `reel1` varchar(255) NOT NULL,
+  `reel2` varchar(255) NOT NULL,
+  `comp1` varchar(255) NOT NULL,
+  `comp2` varchar(255) NOT NULL,
   `id_user` int(4) NOT NULL,
   `date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -85,11 +121,25 @@ INSERT INTO `users` (`id`, `login`, `password`) VALUES
 --
 
 --
+-- Index pour la table `crypto`
+--
+ALTER TABLE `crypto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `crypto_ibfk_1` (`id_user`);
+
+--
 -- Index pour la table `history`
 --
 ALTER TABLE `history`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `polynomial`
+--
+ALTER TABLE `polynomial`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `polynomial_ibfk_1` (`id_user`);
 
 --
 -- Index pour la table `users`
@@ -105,23 +155,41 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pour la table `polynomial`
+--
+ALTER TABLE `polynomial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
+-- Contraintes pour la table `crypto`
+--
+ALTER TABLE `crypto`
+  ADD CONSTRAINT `crypto_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+
+--
 -- Contraintes pour la table `history`
 --
 ALTER TABLE `history`
   ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `polynomial`
+--
+ALTER TABLE `polynomial`
+  ADD CONSTRAINT `polynomial_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
