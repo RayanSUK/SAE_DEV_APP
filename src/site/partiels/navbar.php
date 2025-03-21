af4500052f6f6280ca812612ae5dbfa1c99dd5ec
Pierre-Alix <126257778+Pierre-Alix@users.noreply.github.com>
2025-03-21 06:20:17 +0100

    Suppression du title
    permet de mettre le titre que l'on veut sur les autres pages

--- src/site/partiels/navbar.php
+++ src/site/partiels/navbar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap">
 
@@ -17,11 +17,11 @@
             <img src="images/sigmaXlogo.png" alt="Le logo du site SigmaX" class="logo img-responsive">
         <ul>
             <li><a href="accueil.php">Accueil</a></li>
-            <li><a href="LoiInverseGaussienne.php">Module probabilité</a></li>
-            <li><a href="module2.php">Module Crypto</a></li>
-            <li><a href="module3.php">Module polynôme</a></li>
+            <li><a href="LoiInverseGaussienne.php">Module Probabilités</a></li>
+            <li><a href="module2.php">Module Crypto.</a></li>
+            <li><a href="module3.php">Module polynômes</a></li>
             <li><a href="history.php">Historique des résultats</a></li>
-            <li><a href="deconnexion.php">Deconnexion</a></li>
+            <li><a href="deconnexion.php">Déconnexion</a></li>
             
         </ul>
         <div class="clearfix"></div>
