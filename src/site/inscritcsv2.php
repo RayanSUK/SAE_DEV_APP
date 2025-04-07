9fcb794ef487c42c85119b9abc521e0f8b5f92af
younes <younesazirgui@gmail.com>
2025-04-07 22:22:14 +0200

    supression du css pour eviter un conflit de css

--- src/site/inscritcsv2.php
+++ src/site/inscritcsv2.php
@@ -83,15 +83,7 @@ mysqli_close($cnx);
 <!--------------------------------------------------------------------------STYLE ------------------------------------------------------------------------------------------------------------->
 <style>
 
-    body, html {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        font-family: Arial, sans-serif;
-        background-color: #f4f4f4;
-        display: flex;
-        flex-direction: column;
-    }
+    
 
 
 
